af40a1c99d24ce48c782e56fba402303e9e2de97
Sherwin Nofuente <shernof@sherwins-mbp.lan>
2019-10-08 00:09:14 +0800

    aa

--- Complex-app/models/Post.js
+++ Complex-app/models/Post.js
@@ -1,5 +1,6 @@
 const postCollection = require('../db').db().collection('posts')
 const ObjectID = require('mongodb').ObjectID
+const User = require('./User')
 
 let Post = function(data, userid){
     this.data = data
@@ -44,19 +45,56 @@ Post.prototype.create = function() {
     })
 }
 
+Post.reusablePostQuery = function(uniqueOperations) {
+    return new Promise(async function(resolve, reject){
+        let aggOperations = uniqueOperations.concat([
+            {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
+            {$project: {
+                title: 1,
+                body: 1,
+                createdDate: 1,
+                author: {$arrayElemAt: ["$authorDocument", 0]}
+            }}
+        ])
+
+        let posts = await postCollection.aggregate(aggOperations).toArray()
+        //  cleanUp author proerty in each post object.
+        posts = posts.map(function(post){
+            post.author = {
+                username: post.author.username,
+                avatar: new User(post.author, true).avatar
+            }
+            return post
+        })
+        resolve(posts)
+    })
+}
+
 Post.findSingleById = function(id) {
     return new Promise(async function(resolve, reject){
         if(typeof(id) != "string" || !ObjectID.isValid(id)) {
             reject()
             return
         }
-        let post = await postCollection.findOne({_id: new ObjectID(id)})
-        if(post){
-            resolve(post)
+        let posts = await Post.reusablePostQuery([
+            {$match: {_id: new ObjectID(id)}}
+        ])
+
+        
+        if(posts.length){
+            console.log(posts[0])
+            resolve(posts[0])
         }else {
             reject()
         }
     })
+}
+
+Post.findByAuthorId = function(authorId){
+    return Post.reusablePostQuery([
+        {$match: {author: authorId}},
+        {$sort: {createdDate: -1}}
+    ])
 }
 
 module.exports = Post
--- Complex-app/router.js
+++ Complex-app/router.js
@@ -11,6 +11,9 @@ router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 
 // post related routes
+router.get('/profile/:username', userController.ifUserExists, userController.profilePostScreen)
+
+// post related routes
 router.get('/create-post', userController.userMustBeLoggedIn, postController.viewCreateScreen)
 router.post('/create-post', userController.userMustBeLoggedIn, postController.create)
 router.get('/post/:id', postController.viewSingle)
